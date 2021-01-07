423da44d983d57ed9f9542b62520f9c54ba05111
James <james.hou.23@bishops.com>
2021-01-07 11:11:29 -0800

    hw fix

--- code.js
+++ code.js
@@ -1,8 +1,21 @@
 mainDiv = document.getElementById("content");
-function multiply() {
+function divide() {
   var a = document.getElementById("fnum").value;
   var b = document.getElementById("snum").value;
+  var display = document.getElementById("display").checked
+  var intdiv = document.getElementById("intdiv").checked
 
-  mainDiv.innerHTML = "<p>The product of " + a + " and " + b + ":";
-  mainDiv.innerHTML += a*b + "</p>";
+  if(intdiv) {
+    var quotient = parseInt(a/b)
+  }
+  else {
+    var quotient = a/b
+  }
+  if(display){
+    mainDiv.innerHTML = "<p>The quotient of " + a + " and " + b + ":";
+  }
+  else {
+    mainDiv.innerHTML += "<p>The quotient of " + a + " and " + b + ":";
+  }
+  mainDiv.innerHTML += quotient + "</p>";
 }
