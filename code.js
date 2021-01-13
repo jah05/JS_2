4738ab2e7d40a1753f22769bca43d77b4413dcf1
James <james.hou.23@bishops.com>
2021-01-13 11:56:13 -0800

    json

--- code.js
+++ code.js
@@ -2,36 +2,60 @@ mainDiv = document.getElementById("content");
 function divide() {
   var a = document.getElementById("fnum").value;
   var b = document.getElementById("snum").value;
-  var display = document.getElementById("display").checked
-  var intdiv = document.getElementById("intdiv").checked
-  var enteredU = document.getElementById("usr").value
-  var enteredP = document.getElementById("pwd").value
-  var usr = "juju"
-  var pwd = "corvette corvette"
+  var display = document.getElementById("display").checked;
+  var intdiv = document.getElementById("intdiv").checked;
+  var enteredU = document.getElementById("usr").value;
+  var enteredP = document.getElementById("pwd").value;
+  var usr = "juju";
+  var pwd = "tiktok";
+  var remember = document.getElementById("mem").checked;
 
+  var data = {
+    "a":a,
+    "b":b,
+    "display": display,
+    "intdiv": intdiv,
+    "enteredU":enteredU,
+    "enteredP":enteredP,
+    "usr":usr,
+    "pwd":pwd,
+    "remember":remember
+  };
 
-  if(intdiv) {
-    var quotient = parseInt(a/b)
+  if(data["intdiv"]) {
+    data["quotient"] = parseInt(data["a"]/data["b"]);
   }
   else {
-    var quotient = a/b
+    data["quotient"] = data["a"]/data["b"];
   }
-  if(usr == enteredU && pwd == enteredP){
-    if(display){
-      mainDiv.innerHTML = "<p>The quotient of " + a + " and " + b + ":";
+  console.log(data["usr"]);
+  console.log(data["pwd"]);
+  console.log(data["enteredU"]);
+  console.log(data["enteredP"]);
+  if(data["usr"] == data["enteredU"] && data["pwd"] == data["enteredP"]){
+    if(data["display"]){
+      mainDiv.innerHTML = "<p>The quotient of " + data["a"] + " and " + data["b"] + ":";
     }
     else {
-      mainDiv.innerHTML += "<p>The quotient of " + a + " and " + b + ":";
+      mainDiv.innerHTML += "<p>The quotient of " + data["a"] + " and " + data["b"] + ":";
     }
-    mainDiv.innerHTML += quotient + "</p>";
+    mainDiv.innerHTML += data["quotient"] + "</p>";
   }
   else {
     mainDiv.innerHTML = "Invalid password";
   }
-  localStorage.setItem("ans", quotient)
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(JSON.stringify(data));
 }
 
-function fill() {
-  var fnum = document.getElementById("fnum")
-  fnum.value = localStorage.getItem("ans");
+function autofill() {
+  data = JSON.parse(localStorage.getItem("data"));
+  console.log(data)
+  if(data["remember"]){
+    document.getElementById("fnum").value = data["quotient"];
+    document.getElementById("usr").value = data["enteredU"];
+    document.getElementById("pwd").value = data["enteredP"];
+    document.getElementById("display").value = data["display"];
+    document.getElementById("intdiv").value = data["intdiv"];
+  }
 }
